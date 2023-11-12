e1da211fab2be8073bc1d192daf7a6b12d8cdb70
zlw235789 <108209398+zlw235789@users.noreply.github.com>
2023-11-12 14:20:05 +0800

    Update wifi_configuration.h

--- main/wifi_configuration.h
+++ main/wifi_configuration.h
@@ -29,10 +29,10 @@ static struct {
 #define MDNS_INSTANCE "DAP mDNS"
 //
 
-#define USE_STATIC_IP 1
+#define USE_STATIC_IP 0
 // If you don't want to specify the ip configuration, then ignore the following items.
-#define DAP_IP_ADDRESS 192, 168, 31, 12
-#define DAP_IP_GATEWAY 192, 168, 31, 1
+#define DAP_IP_ADDRESS 192, 168, 35, 12
+#define DAP_IP_GATEWAY 192, 168, 35, 1
 #define DAP_IP_NETMASK 255, 255, 255, 0
 //
 
